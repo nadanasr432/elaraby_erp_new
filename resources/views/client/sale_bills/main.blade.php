f0af24fa2a7124766438afa8650d9fa17ee682e7
nadanasr432 <nadanm432@gmail.com>
2024-07-07 23:41:36 +0300

    fix

--- resources/views/client/sale_bills/main.blade.php
+++ resources/views/client/sale_bills/main.blade.php
@@ -652,7 +652,7 @@
 
                                             {{ $currency }} --}}
                                             <td dir="rtl">
-                                                {{ round($realtotal - $discount->value) + $sumWithTax }}
+                                                {{   $sumWithTax + round($realtotal - $discount->value) }}
                                                 {{ $currency }} </td>
                                         @endif
                                     @else
