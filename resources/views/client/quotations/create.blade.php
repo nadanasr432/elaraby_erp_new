311b5e11002b16478d7743d1e39f45698682ba23
nadanasr432 <nadanm432@gmail.com>
2024-08-07 00:19:48 +0300

    fix

--- resources/views/client/quotations/create.blade.php
+++ resources/views/client/quotations/create.blade.php
@@ -542,146 +542,119 @@
             $('#quantity_price').val(quantity_price);
         });
 
-        //------on adding new qoutation ------//
-        $('#add').on('click', function() {
-            let outer_client_id = $('#outer_client_id').val();
-            let quotation_number = $('#quotation_number').val();
-            let product_id = $('#product_id').val();
-            let product_price = $('#product_price').val();
-            let quantity = $('#quantity').val();
-            let unit_id = $('#unit_id').val();
-            let start_date = $('#start_date').val();
-            let expiration_date = $('#expiration_date').val();
-            let quantity_price = quantity * product_price;
-            let notes = $('#notes').val();
+      $('#add').on('click', function() {
+    let outer_client_id = $('#outer_client_id').val();
+    let quotation_number = $('#quotation_number').val();
+    let product_id = $('#product_id').val();
+    let product_price = $('#product_price').val();
+    let quantity = $('#quantity').val();
+    let unit_id = $('#unit_id').val();
+    let start_date = $('#start_date').val();
+    let expiration_date = $('#expiration_date').val();
+    let quantity_price = quantity * product_price;
+    let notes = $('#notes').val();
 
-            let discount_type = $('#discount_type').val();
-            let discount_value = $('#discount_value').val();
+    let discount_type = $('#discount_type').val();
+    let discount_value = $('#discount_value').val();
 
-            let extra_type = $('#extra_type').val();
-            let extra_value = $('#extra_value').val();
-            let first_balance = parseFloat($('#quantity').attr('max'));
-            if (outer_client_id == "") {
-                alert("لابد ان تختار المورد أولا");
-            } else {
-                if (product_id == "" || product_id <= "0") {
-                    alert("لابد ان تختار المنتج أولا");
-                } else if (product_price == "" || product_price == "0") {
-                    alert("لم يتم اختيار سعر المنتج");
-                } else if (quantity == "" || quantity <= "0") {
-                    alert("الكمية غير مناسبة");
-                } else if (quantity_price == "" || quantity_price == "0") {
-                    alert("الكمية غير مناسبة او الاجمالى غير صحيح");
-                } else if (unit_id == "" || unit_id == "0") {
-                    alert("اختر الوحدة");
-                } else {
-                    $.post("{{ url('/client/quotations/post') }}", {
-                        outer_client_id: outer_client_id,
-                        quotation_number: quotation_number,
-                        product_id: product_id,
-                        product_price: product_price,
-                        quantity: quantity,
-                        unit_id: unit_id,
-                        quantity_price: quantity_price,
-                        start_date: start_date,
-                        notes: notes,
-                        expiration_date: expiration_date,
-                        "_token": "{{ csrf_token() }}"
-                    }, function(data) {
-                        // console.log(data);return;
-                        $('#outer_client_id').attr('disabled', true).addClass('disabled');
-                        $('#product_id').val('').trigger('change');
-                        $('#discount_type').attr('disabled', false);
-                        $('.print_btn').attr('disabled', false);
-                        $('.close_btn').attr('disabled', false);
-                        $('.save_btn').removeClass('disabled');
-                        $('.send_btn').removeClass('disabled');
-                        $('#discount_value').attr('disabled', false);
-                        $('#exec_discount').attr('disabled', false);
-                        $('#extra_type').attr('disabled', false);
-                        $('#extra_value').attr('disabled', false);
-                        $('#exec_extra').attr('disabled', false);
-                        $('#product_price').val('0');
-                        $('#quantity').val('');
-                        $('#unit_id').val('');
-                        $('#quantity_price').val('');
-                        if (data.status == true) {
-                            $('.box_success').removeClass('d-none').fadeIn(200);
-                            $('.msg_success').html(data.msg);
-                            $('.box_success').delay(3000).fadeOut(300);
+    let extra_type = $('#extra_type').val();
+    let extra_value = $('#extra_value').val();
 
-                            //----update elements-----//
-                            $.post("{{ url('/client/quotations/elements') }}", {
-                                "_token": "{{ csrf_token() }}",
-                                quotation_number: quotation_number,
-                                product_id: product_id
-                            }, function(elements) {
-                                $("#saveANDPRINT").append(
-                                    '<input type="hidden" name="quotation_id" value="' +
-                                    quotation_number + '">');
-                                $("#saveANDPRINT").show();
-                                $('.bill_details').html(elements);
-                                $('.bill_details').fadeIn(700);
-                            });
+    if (outer_client_id == "") {
+        alert("لابد ان تختار المورد أولا");
+        return;
+    } 
+    if (product_id == "" || product_id <= "0") {
+        alert("لابد ان تختار المنتج أولا");
+        return;
+    } 
+    if (product_price == "" || product_price == "0") {
+        alert("لم يتم اختيار سعر المنتج");
+        return;
+    } 
+    if (quantity == "" || quantity <= "0") {
+        alert("الكمية غير مناسبة");
+        return;
+    } 
+    if (quantity_price == "" || quantity_price == "0") {
+        alert("الكمية غير مناسبة او الاجمالى غير صحيح");
+        return;
+    } 
+    if (unit_id == "" || unit_id == "0") {
+        alert("اختر الوحدة");
+        return;
+    }
 
-                            //----update discount-----//
-                            $.post("{{ url('/client/quotations/discount') }}", {
-                                "_token": "{{ csrf_token() }}",
-                                quotation_number: quotation_number,
-                                discount_type: discount_type,
-                                discount_value: discount_value
-                            }, function(data) {
-                                $('.after_totals').html(data);
-                            });
+    $.post("{{ url('/client/quotations/post') }}", {
+        outer_client_id: outer_client_id,
+        quotation_number: quotation_number,
+        product_id: product_id,
+        product_price: product_price,
+        quantity: quantity,
+        unit_id: unit_id,
+        quantity_price: quantity_price,
+        start_date: start_date,
+        expiration_date: expiration_date,
+        notes: notes,
+        "_token": "{{ csrf_token() }}"
+    }, function(data) {
+        if (data.status) {
+            // Success actions
+            $('#outer_client_id').attr('disabled', true).addClass('disabled');
+            $('#product_id').val('').trigger('change');
+            $('#discount_type, #discount_value, #extra_type, #extra_value, #exec_discount, #exec_extra').attr('disabled', false);
+            $('.print_btn, .close_btn, .save_btn, .send_btn').attr('disabled', false).removeClass('disabled');
+            $('#product_price, #quantity, #unit_id, #quantity_price').val('');
 
-                            //----update shipping-----//
-                            $.post("{{ url('/client/quotations/extra') }}", {
-                                "_token": "{{ csrf_token() }}",
-                                quotation_number: quotation_number,
-                                extra_type: extra_type,
-                                extra_value: extra_value
-                            }, function(data) {
-                                $('.after_totals').html(data);
-                            });
+            $('.box_success').removeClass('d-none').fadeIn(200).delay(3000).fadeOut(300);
+            $('.msg_success').html(data.msg);
 
-                        } else {
-                            $('.box_error').removeClass('d-none').fadeIn(200);
-                            $('.msg_error').html(data.msg);
-                            $('.box_error').delay(3000).fadeOut(300);
+            updateQuotationElements(quotation_number);
+            updateDiscount(quotation_number, discount_type, discount_value);
+            updateExtra(quotation_number, extra_type, extra_value);
+        } else {
+            // Error actions
+            $('.box_error').removeClass('d-none').fadeIn(200).delay(3000).fadeOut(300);
+            $('.msg_error').html(data.msg);
 
-                            //----update elements-----//
-                            $.post("{{ url('/client/quotations/elements') }}", {
-                                "_token": "{{ csrf_token() }}",
-                                quotation_number: quotation_number
-                            }, function(elements) {
-                                $('.bill_details').html(elements);
-                                $('.bill_details').fadeIn(700);
-                            });
+            updateQuotationElements(quotation_number);
+            updateDiscount(quotation_number, discount_type, discount_value);
+            updateExtra(quotation_number, extra_type, extra_value);
+        }
+    });
+});
 
-                            //----update discount-----//
-                            $.post("{{ url('/client/quotations/discount') }}", {
-                                "_token": "{{ csrf_token() }}",
-                                quotation_number: quotation_number,
-                                discount_type: discount_type,
-                                discount_value: discount_value
-                            }, function(data) {
-                                $('.after_totals').html(data);
-                            });
+function updateQuotationElements(quotation_number) {
+    $.post("{{ url('/client/quotations/elements') }}", {
+        "_token": "{{ csrf_token() }}",
+        quotation_number: quotation_number
+    }, function(elements) {
+        $('.bill_details').html(elements).fadeIn(700);
+    });
+}
 
-                            //----update shipping-----//
-                            $.post("{{ url('/client/quotations/extra') }}", {
-                                "_token": "{{ csrf_token() }}",
-                                quotation_number: quotation_number,
-                                extra_type: extra_type,
-                                extra_value: extra_value
-                            }, function(data) {
-                                $('.after_totals').html(data);
-                            });
-                        }
-                    });
-                }
-            }
-        });
+function updateDiscount(quotation_number, discount_type, discount_value) {
+    $.post("{{ url('/client/quotations/discount') }}", {
+        "_token": "{{ csrf_token() }}",
+        quotation_number: quotation_number,
+        discount_type: discount_type,
+        discount_value: discount_value
+    }, function(data) {
+        $('.after_totals').html(data);
+    });
+}
+
+function updateExtra(quotation_number, extra_type, extra_value) {
+    $.post("{{ url('/client/quotations/extra') }}", {
+        "_token": "{{ csrf_token() }}",
+        quotation_number: quotation_number,
+        extra_type: extra_type,
+        extra_value: extra_value
+    }, function(data) {
+        $('.after_totals').html(data);
+    });
+}
+
 
         //----exec discount-----//
         $('#exec_discount').on('click', function() {
